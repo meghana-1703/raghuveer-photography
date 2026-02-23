72e120460bc49111bd398a356cbf772ae7c4d8a0
Muduthanapally Meghana <m.meghana1225@gmail.com>
2026-02-23 18:08:53 +0530

    Updated layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,11 @@ const cormorant = Cormorant_SC({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
 });
-
+export const metadata = {
+  verification: {
+    google: "Gn8du1nZDsuSZsy7ujV-2rRhdiyTp4N2lfNEevoUcOE",
+  },
+};
 export default function RootLayout({
   children,
 }: {
